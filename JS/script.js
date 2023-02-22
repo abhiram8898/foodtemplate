c78151be2b26a2d667eda0025725a856590fe77e
Abhiram <abhiravi889833@gmail.com>
2023-02-22 18:00:59 +0530

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,22 @@
-
+function validate() {
+    var name = document.getElementById("namee").value;
+    email = document.getElementById("email").value;
+    let reg = /^[a-z A-Z]{2,15}$/;
+    var e =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (name == "") {
+        alert("Name is empty");
+        return false;
+    } else if (!reg.test(name)) {
+        alert("name is  not character ");
+        return false;
+    } else if (email == "") {
+        alert("enter mail");
+        return false;
+    } else if (!e.test(email)) {
+        alert("enter valid mail addres");
+        return false;
+    } else {
+        return true;
+    }
+}
